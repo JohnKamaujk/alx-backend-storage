21a42b5b33e02d95921cd2f8b87eb22d94a9eb01
JohnKamaujk <johnjkkamau@gmail.com>
2024-01-17 18:09:11 +0300

    :white_check_mark:Complete:Create users table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,8 @@
 -- Create the 'users' table if it does not exist
 
-CREATE TABLE IF NOT EXISTS users (
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
-);
+)
