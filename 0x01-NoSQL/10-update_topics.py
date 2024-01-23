5f2abd2a6a21e8f9e53b7aadbcdfa716792dc914
JohnKamaujk <johnjkkamau@gmail.com>
2024-01-23 19:24:50 +0300

    :white_check_mark:Complete:Update existing collections

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-""" 10-module update topics module"""
+'''Task 10's module.
+'''
 
 
 def update_topics(mongo_collection, name, topics):
-    """ Changes all topics of a school document """
+    '''Changes all topics of a collection's document based on the name.
+    '''
     mongo_collection.update_many(
-        {name: name},
-        {"$set": {topics: topics}}
+        {'name': name},
+        {'$set': {'topics': topics}}
     )
