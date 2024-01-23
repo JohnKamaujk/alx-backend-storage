9d1803678f6d411a0fb6050d0e51794612a13f07
JohnKamaujk <johnjkkamau@gmail.com>
2024-01-23 18:40:07 +0300

    :white_check_mark:Complete:py script to find documents

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-""" 11-schools by topics module"""
+'''Task 11's module.
+'''
 
 
 def schools_by_topic(mongo_collection, topic):
-    """ Returns the list of school having a specific topic."""
-    documents = mongo_collection.find(
-        {{"topics": {"$in": [topic]}}},
-    )
-    return list(documents)
+    """ Returns the list of schools having a specific topic """
+    topic_filter = {'topics': {'$elemMatch': {'$eq': topic}}}
+    return [doc for doc in mongo_collection.find(topic_filter)]
